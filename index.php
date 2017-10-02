35927f7634af75fc8665ba753ac9a79e43f72a93
Takeyuki Ueda <gde00107@nifty.com>
2017-10-03 08:20:55 +0900

    fist commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf8">
+    <title>pic a book</title>
+  
+	  <!-- BOOTSTRAP start -->
+	  <!-- Latest compiled and minified CSS -->
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	  <!-- <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">-->
+	  <!-- Optional theme -->
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	  <!-- <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">-->
+	  <!-- Latest compiled and minified JavaScript -->
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	  <!-- <script src="node_modules/bootstrap/dist/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>-->
+	  <!-- BOOTSTRAP end -->
+
+		<SCRIPT LANGUAGE="JavaScript">
+      function addBarCord() {
+        // launch pic2shop and tell it to open Google Products with scan result
+	      window.location="pic2shop://scan?callback=http%3A//klingsor.uedasoft.com/tools/171002/index.php%3Fcommand%3Dadd%26barcord%3DEAN";
+      }
+      function addQRCord() {
+        // launch pic2shop and tell it to open Google Products with scan result
+	      window.location="pic2shop://scan?callback=http%3A//klingsor.uedasoft.com/tools/171002/index.php%3Fcommand%3Dadd%26barcord%3DQR";
+      }
+    </SCRIPT>
+  </head>
+  <body>
+  	<?php if (isset($_GET['command'])&&($_GET['command'] == "add")): ?>
+  		<?php if (isset($_GET['barcord'])): ?>
+  			<?= $_GET['barcord'] ?>
+			<?php endif; ?>
+		<?php endif; ?>
+		<FORM>
+      <INPUT TYPE=BUTTON OnClick="addBarCord();" VALUE="Scan Barcode">
+    </FORM>
+		<FORM>
+      <INPUT TYPE=BUTTON OnClick="addQRCord();" VALUE="Scan QRcode">
+    </FORM>
+  </body>
+</html>
