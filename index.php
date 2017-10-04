02b4b76443cf4a4b2f71edcd903f1e078c799846
Takeyuki Ueda <gde00107@nifty.com>
2017-10-04 23:00:07 +0900

    l&f

--- index.php
+++ index.php
@@ -15,24 +15,26 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
   if (isset($_GET['command'])&&isset($_GET['barcord'])&&($_GET['command']=="addISBN")){
     // read bibliographicals from NDL and put it on the FORM.
     $ndl = new NDLsearch($_GET['barcord']);
-    $_SESSION["title"] = (string)$ndl->title();
-    $_SESSION["creator"] = (string)$ndl->creator();
-    $_SESSION["publisher"] = (string)$ndl->publisher();
+    $_SESSION["isbn"]       = $_GET['barcord'];
+    $_SESSION["title"]      = (string)$ndl->title();
+    $_SESSION["creator"]    = (string)$ndl->creator();
+    $_SESSION["publisher"]  = (string)$ndl->publisher();
   }
   if (isset($_GET['command'])&&isset($_GET['barcord'])&&($_GET['command']=="addJAN")){
     // read the price and put it on the FORM.
-    $_SESSION["price"] = substr($_GET['barcord'],7,5);
+    $_SESSION["price"] = intval(substr($_GET['barcord'],7,5))."円";
   }
 }elseif($_SERVER["REQUEST_METHOD"] == "POST"){
   $date = new DateTime();
 
   $list = array(
-    $date->format('Y-m-d H:i:s'),$_SESSION["title"], $_SESSION["creator"], $_SESSION["publisher"], $_SESSION["price"]
+    $date->format('Y-m-d H:i:s'),$_POST["isbn"],$_POST["title"], $_POST["creator"], $_POST["publisher"], $_POST["price"], $_POST["memo"]
   );
-  $fp = fopen('data/bibliography.txt','w');
+  $fp = fopen('data/bibliography.csv','a');
   rewind ($fp);
   fputcsv($fp,$list);
   fclose($fp);
+  unset($_SESSION["isbn"]);
   unset($_SESSION["title"]);
   unset($_SESSION["creator"]);
   unset($_SESSION["publisher"]);
@@ -40,6 +42,8 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
 }else{
 
 }
+
+$isSubmitEnabled = isset($_SESSION["isbn"])&&isset($_SESSION["price"]);
 
 ?>
 
@@ -49,7 +53,7 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
     <meta charset="utf8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    <title>pic a book</title>
+    <title>pick a book</title>
   
 	  <!-- JQM 1.3 start -->
   	<link rel="stylesheet" href="https://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css" />
@@ -79,29 +83,44 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
     </SCRIPT>
   </head>
   <body>
-<!--  	<?php if (isset($_GET['command'])&&($_GET['command'] == "add")): ?>
-  		<?php if (isset($_GET['barcord'])): ?>
-  			<?= $_GET['barcord'] ?>
-			<?php endif; ?>
-		<?php endif; ?> -->
-    <FORM>
-      <INPUT TYPE=BUTTON OnClick="readISBNCord();" VALUE="ISBNコードのスキャン">
-    </FORM>
-    <FORM>
-      <INPUT TYPE=BUTTON OnClick="readJANCord();" VALUE="JANコードのスキャン">
-    </FORM>
-    <form action="<?= $_SERVER['SCRIPT_NAME']; ?>" method="post" data-ajax="false">
-      <input type="text" name="titel" id="title" value="<?= isset($_SESSION["title"])?$_SESSION["title"]:"" ?>" />
-      <input type="text" name="creator" id="creator" value="<?= isset($_SESSION["creator"])?$_SESSION["creator"]:"" ?>" />
-      <input type="text" name="publisher" id="publisher" value="<?= isset($_SESSION["publisher"])?$_SESSION["publisher"]:"" ?>" />
-      <input type="text" name="price" id="price" value="<?= isset($_SESSION["price"])?$_SESSION["price"]:"" ?>" />
-      <input type="submit" value="登録" />
-    </form>
-<!--		<FORM>
-      <INPUT TYPE=BUTTON OnClick="addBarCord();" VALUE="Scan Barcode">
-    </FORM>
-		<FORM>
-      <INPUT TYPE=BUTTON OnClick="addQRCord();" VALUE="Scan QRcode">
-    </FORM> -->
+
+    <div data-role="page">
+    
+      <div data-role="header" data-position="fixed" data-disable-page-zoom="false">
+        <h1>Pick a book</h1>
+      </div> <!-- header -->
+
+      <div data-role="content">
+
+        <FORM>
+          <INPUT TYPE=BUTTON OnClick="readISBNCord();" VALUE="ISBNコードのスキャン">
+        </FORM>
+        <FORM>
+          <INPUT TYPE=BUTTON OnClick="readJANCord();" VALUE="JANコードのスキャン">
+        </FORM>
+        <form action="<?= $_SERVER['SCRIPT_NAME']; ?>" method="post" data-ajax="false">
+          <input type="hidden" name="isbn"      id="isbn"       value="<?= isset($_SESSION["isbn"])?$_SESSION["isbn"]:"" ?>" />
+          <input type="hidden" name="titel"     id="title"      value="<?= isset($_SESSION["title"])?$_SESSION["title"]:"" ?>" />
+          <input type="hidden" name="creator"   id="creator"    value="<?= isset($_SESSION["creator"])?$_SESSION["creator"]:"" ?>" />
+          <input type="hidden" name="publisher" id="publisher"  value="<?= isset($_SESSION["publisher"])?$_SESSION["publisher"]:"" ?>" />
+          <input type="hidden" name="price"     id="price"      value="<?= isset($_SESSION["price"])?$_SESSION["price"]:"" ?>" />
+            ISBN: <?= isset($_SESSION["isbn"])?$_SESSION["isbn"]:"" ?><br>
+            書名: <?= isset($_SESSION["title"])?$_SESSION["title"]:"" ?><br>
+            価格: <?= isset($_SESSION["price"])?$_SESSION["price"]:"" ?><br>
+          <input type="text"    <?= $isSubmitEnabled? '' : 'disabled="disabled"' ?> name="memo" id="memo" placeholder="メモ"/>
+          <input type="submit"  <?= $isSubmitEnabled? '' : 'disabled="disabled"' ?> value="登録" />
+        </form>
+    <!--		<FORM>
+          <INPUT TYPE=BUTTON OnClick="addBarCord();" VALUE="Scan Barcode">
+        </FORM>
+    		<FORM>
+          <INPUT TYPE=BUTTON OnClick="addQRCord();" VALUE="Scan QRcode">
+        </FORM> -->
+      </div> <!-- content -->
+
+      <div data-role="footer" data-position="fixed" data-disable-page-zoom="false">
+        <h4>© Atelier UEDA <img src="favicon.ico"></h4>
+      </div>
+    </div> <!-- page -->
   </body>
 </html>
